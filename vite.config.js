e4b77fcf8db71034df9c170f98be325be7fbd212
Anil Panth <anilpanth44@gmail.com>
2025-01-14 00:08:43 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-/* eslint-disable no-undef */
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'), // Set @ to point to the src directory
     },
   },
 });
